773b36ca6821c714ba6df93fa9548e4e7249b909
jhalauday <45385828+jhalauday@users.noreply.github.com>
2018-11-27 16:02:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
 if __name__ == "__main__":
     app.run(debug=True)

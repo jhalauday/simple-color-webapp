5032204449b8ea1b53a510ed022bf4d7a40c9beb
jhalauday <45385828+jhalauday@users.noreply.github.com>
2018-11-28 14:34:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue"])
 @app.route("/")
 def main():
     print(color)
-	return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
 @app.route('/color/<new_color>')
 def new_color(new_color):
-	return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+     return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)

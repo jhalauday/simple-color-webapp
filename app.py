6b848a074d8eba08e31e7322cde94384ae2ce835
jhalauday <45385828+jhalauday@users.noreply.github.com>
2018-11-28 13:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import os
 from flask import Flask
+from flask import render_template
+import socket
+import random
+import os
+
 app = Flask(__name__)
+
+color_codes = {
+	"red": "#e74c3c",
+	"green": "#16a085",
+	"blue": "#2980b9"
+	}
+	
+	color = os.environ.get('APP_COLOR') or random.choice("red","green","blue"])
+
+
 @app.route("/")
 def main():
-    return "Hello, Durgesh!"
+    print(color)
+	return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
-@app.route('/how are you')
-def hello():
-     return 'I am good, how are you?'
+@app.route('/color/<new_color>')
+def new_color(new_color):
+	return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
